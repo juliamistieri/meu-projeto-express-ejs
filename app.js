cd332df41ad6e47c57549338eee1e54430a27f82
juliamistieri <julia.mistieri02@gmail.com>
2023-11-01 09:35:13 -0400

    estilizei
    estilização

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produto = [
+  {id: 1, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 2, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 3, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 4, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 5, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 6, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 7, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 8, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 9, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 10, nome:"", preco:"", descricao:"", imagem:""}
+]
+
 app.get('/', (req, res) => { //controller/rota    get é o verbo HTTP   req é o que entra no servidor (requisição) == a resposta é res (o que sai)
-  res.render('index', { message: 'Olá, Mundo!' });   //
+  res.render('index', { message: 'LISTA DE PRODUTOS' });   //
 });
 
-app.get('/contatos', (req, res) => { 
-  res.render('contatos', { message: 'Esta é a página de contato.' });   //
+app.get('/produtos', (req, res) => { 
+  res.render('produtos', { message: 'LISTA DE PRODUTOS' });   //
 });
 
 app.listen(port, () => {
