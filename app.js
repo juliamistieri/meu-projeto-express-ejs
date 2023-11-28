531fb456e88777efebdb475ac65c7fce23433baa
juliamistieri <julia.mistieri02@gmail.com>
2023-11-28 08:33:24 -0400

    css, app e index
    alinhei css, removi produto e acrescentei os reais

--- app.js
+++ app.js
@@ -6,17 +6,35 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id: 1, nome:"trembalinha", preco:"3243", descricao:"fdgf", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 2, nome:"j", preco:"299", descricao:"kk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 3, nome:"j", preco:"324", descricao:"kkk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 4, nome:"j", preco:"356", descricao:"kkkk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 5, nome:"j", preco:"215", descricao:"kkkkk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 6, nome:"j", preco:"5677", descricao:"kkk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 7, nome:"j", preco:"1432", descricao:"k", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 8, nome:"j", preco:"453", descricao:"kkkuj", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 9, nome:"j", preco:"2435", descricao:"kkk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  {id: 10, nome:"j", preco:"345", descricao:"khgjgvk", imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
-  
+  {id: 1,
+     nome:"Jaqueta Nike X NOCTA Tech Fleece Full-Zip Black University Gold",
+      preco:"899,00",
+       descricao:"Combinando 2 de nossos looks mais icônicos, este moletom com capuz de zíper completo foi inspirado em nossa jaqueta atemporal Windrunner, bem como em nossa jaqueta Tech Fleece.",
+        imagem:"https://imgnike-a.akamaihd.net/768x768/026942ID.jpg"},
+  {id: 2,
+     nome:"Polo Nike Court Dri-FIT Masculina",
+      preco:"299,90",
+       descricao:"Feita de tecido de malha macio com 100% de fibras de poliéster reciclado, a Polo NikeCourt Dri-FIT é uma peça para as quadras de look clássico e material antissuor.",
+        imagem:"https://imgnike-a.akamaihd.net/768x768/01363251.jpg"},
+  {id: 3,
+     nome:"Tênis Nike Air Force 1 07",
+   preco:"799,90",
+    descricao:"O brilho perdura no Nike Air Force 1 ’07, o tênis original do basquete que dá um toque de inovação naquilo que você conhece bem: sobreposições costuradas e duráveis, acabamentos simples e a quantidade perfeita de brilho para fazer você se destacar.",
+     imagem:"https://imgnike-a.akamaihd.net/768x768/01113751.jpg"},
+  {id: 4,
+     nome:"Sideline Jacket HD",
+      preco:"999,90",
+       descricao:"Proteja-se do clima frio e úmido com o NOCTA. Impermeável e com isolamento, esta jaqueta foi feita para mantê-lo seco e aquecido, dia e noite.",
+        imagem:"https://imgnike-a.akamaihd.net/768x768/0277733X.jpg"},
+  {id: 5,
+     nome:"Nike x PEACEMINUSONE G-Dragon",
+   preco:"2999,90",
+    descricao:"Apenas tente encontrar um visual de verão mais exclusivo. G-Dragon canaliza seu passado militar e seu amor pelo taekwondo com esta combinação de jaqueta e colete em camadas. Use-os juntos, com o colete isolante como sobreposição, ou use separado. A escolha é sua.",
+     imagem:"https://imgnike-a.akamaihd.net/768x768/026217IDA2.jpg"},
+  {id: 6, nome:"Nike x Jacquemus",
+   preco:"799,90",
+    descricao:"A moda das passarelas encontra o estilo streetwear nesta versão da polo clássica. Detalhes de tiras nas costas abertas acentuam seu visual, enquanto o tecido canelado destaca a forma sofisticada do design.",
+     imagem:"https://imgnike-a.akamaihd.net/768x768/0254113X.jpg"}
 ]
 
 function buscarProdutoPorID(id){
